ee47782902df413c8f870e8dc1c5fb25424f74a0
Kemp <653009@trinity.vic.edu.au>
2025-09-29 15:32:17 +1000

    how

--- gamejamcode.js
+++ gamejamcode.js
@@ -284,7 +284,7 @@ function keyPressed() {
     speedBoostOn = true;
     isInvisible = false;
   } else if (keyCode === 73 && invisibility>0) {
-    playerState = "invisible"
+    playerState = "invisible";
     isInvisible=true;
     speedBoostOn=false;
   }
@@ -506,13 +506,13 @@ function drawHowPage() {
   textSize(small);
   text("Left/right arrow keys or A/D to move\n\nI key to turn invisible\n\nF key to speed boost\n\nYou can't be invisible and have speed boost at the same timeF key to speed boost\n\nThe percentage is the chance that each thing will kill you. Jellies have the lowest chance, followed by fish, and then coral. Rocks definitely kill you\n\n\n\nTry to reach the end with the fastest time", 100, 100, 400, 500);
   
-  image(jelly, 100, 400, 40, 40);
+  image(jelly, 100, 450, 40, 40);
  
-  image(fish, 140, 400, 40, 40);
+  image(fish, 140, 450, 40, 40);
  
 
-  image(coral, 180, 400, 40, 40);
- image(rock, 220, 400, 40, 40);
+  image(coral, 180, 450, 40, 40);
+ image(rock, 220, 450, 40, 40);
   
   fill(255);
   rect(backButtonX, backButtonY, backButtonWidth, backButtonHeight);
